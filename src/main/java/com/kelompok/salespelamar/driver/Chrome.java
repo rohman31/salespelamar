c4ebe3c01bac8b388e3cfa1eac960aa28a4c55d6
rohmanrizki <rohmanrizki29@gmail.com>
2022-06-26 23:46:55 +0700

    Tambahan

--- src/main/java/com/kelompok/salespelamar/driver/Chrome.java
+++ src/main/java/com/kelompok/salespelamar/driver/Chrome.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Chrome implements DriverStrategy {
 
 	public WebDriver setStrategy() {
-//		System.setProperty("webdriver.chrome.driver", "/Users/rohman/Documents/chromedriver");
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe"); 
+		System.setProperty("webdriver.chrome.driver", "/Users/rohman/Documents/chromedriver");
+//		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+//		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe"); 
 		WebDriver driver = new ChromeDriver();
 		return driver;
 	}
--- src/main/resources/features/2 DataPelamar.feature
+++ src/main/resources/features/2 DataPelamar.feature
@@ -1,6 +1,6 @@
 Feature: Customer Login ke Web Salesindo Pelamar
 	Scenario: Testing Data Pelamar 
-		When Customer klik button klik disini
+		When Customer klik button klik disinii
 		Then Customer berhasil masuk ke form data pelamar
 		
 	
